d806ad847947c32aa951997e39d14ceddfd33955
joseph <josp2013@gmail.com>
2022-06-02 16:58:03 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char *p = "with proper grammar, but the outcome is a piece of art,";
+char *p = "with proper grammar, but the outcome is a piece of art, ";
 
 printf("%s", p);
 return (0);
