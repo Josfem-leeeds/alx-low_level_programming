8d83e18fc0518b05d8f1af0810460bd5f318f062
joseph <josp2013@gmail.com>
2022-06-04 14:22:53 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - this is the main code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+	putchar('\n');
+
+	return (0);
+}
