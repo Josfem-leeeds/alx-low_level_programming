a24506e38003bea7fe74d7d24739fbb3b20596e6
joseph <josp2013@gmail.com>
2022-06-04 13:25:10 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the code starts from here
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
