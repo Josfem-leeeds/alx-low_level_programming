6b158f5e49d1739cdeebd4bb365ef7b0d0cb7399
joseph <josp2013@gmail.com>
2022-06-13 07:46:02 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * Main - This function prints lower alphabets 10 times
+ *
+ * Description: Using the for function
+ *
+ * Return: Always 0 Success
+ */
+void print_alphabet_x10(void)/* Function declaration */
+{
+	char l;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (l = 'a'; i <= 'z'; i++)
+		{
+		_putchar(l);
+		}
+		_putchar('\n');
+	}
+}
