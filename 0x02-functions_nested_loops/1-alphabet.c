0a57f09363bbbc73e4404e74f0ed0293b4a0a87a
joseph <josp2013@gmail.com>
2022-06-13 04:21:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print-alphabet writes all the lower character alphabet
+ * Return: it returns 0 meaning successful.
+ */
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+	}
+	putchar ('\n');
+}
