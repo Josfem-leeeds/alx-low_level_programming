39b9e318d6979ad7f62efbcd2833a49f27caf9a3
joseph <josp2013@gmail.com>
2022-06-14 16:00:56 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the number for themselves
+ * @a: a pointer the integer we want to set to 42
+ * @b: a pointer the integer we want to set to 98
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
