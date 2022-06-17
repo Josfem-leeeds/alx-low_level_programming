d3ab059110e077b2c6193a9939f820a5b9aa9988
joseph <josp2013@gmail.com>
2022-06-16 17:56:05 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * *_strcat - Appendsa string to another string
- *@dest: the destinated string to be appended to.
- *@src: the string to be appended
+ * @dest: the destinated string to be appended to.
+ * @src: the string to be appended
  *Return: Returns a pointer pointing to the resulting string destination
  */
 char *_strcat(char *dest, char *src)
