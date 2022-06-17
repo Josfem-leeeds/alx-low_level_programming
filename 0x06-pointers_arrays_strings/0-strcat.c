4a06ea4435772eddf5411c70e14f4771fd39ff7f
joseph <josp2013@gmail.com>
2022-06-16 17:18:38 -0700

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,5 +8,28 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int i = 0, j = 0;
+	char s1[100];
+	char s2[100];
+	char s3[100];
+	dest = &s1;
+	src = &s2;
+	char *ptr = &s3;
+
+	while (s1[i] != '\0')
+	{
+		s3[j] = s1[i];
+		i++;
+		j++;
+	}
+
+	i= 0 ;
+	while (s2[i] != '\0')
+	{
+		s3[j] = s2[i];
+		i++;
+		j++;
+	}
+	s3[j] = '\0';
+	return (s3);
 }
