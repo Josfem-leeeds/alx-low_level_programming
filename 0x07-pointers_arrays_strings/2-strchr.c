e9dfa7ff1291c65b31ada9e3e5feba676e3ea1ea
joseph <josp2013@gmail.com>
2022-06-21 18:14:04 -0700

    locator located locally

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - the function returns a pointer to the first occurence of char c
+ * @s: the first parameter pointer
+ * @c: the second parameter to be checked
+ * Return: returns a pointer
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+
+		if (*s == c)
+		return (s);
+	} while (s++);
+	return (0);
+}
