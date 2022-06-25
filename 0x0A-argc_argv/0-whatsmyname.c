9baf25fde1ec925ac28a379a1276b8ad91749500
Josfem-leeds <josp2013@gmail.com>
2022-06-24 19:59:46 -0700

    error fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: the argument strings
  * Return: Returns and integer value on success
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
