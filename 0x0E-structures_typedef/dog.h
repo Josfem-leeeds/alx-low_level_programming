e134283e9cb57345a5bb99a256d2ad1f2ddad436
Josfem-leeds <josp2013@gmail.com>
2022-07-04 22:25:34 -0700

    0-Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+/**
+ * struct dog - Details of the struct dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's opwner
+ *
+ * Description: this dog struct indicates the name,
+ * age and owner of the dog structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
