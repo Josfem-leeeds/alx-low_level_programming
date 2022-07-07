c571d2ff0d79f28b0787f0f937b4e96dd9847a6e
Josfem-leeds <josp2013@gmail.com>
2022-07-07 11:26:25 -0700

    whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints name
+ * @name: first parameter
+ * @f: function pointer
+ * 
+ * Return: this returns 0 success
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+# C-FUNCTION POINTERS
+It's possible to take the address of a function, too. And, similarly to arrays, functions decay to pointers when their names are used.
+
