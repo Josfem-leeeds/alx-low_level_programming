1b12b985c584271ea4319d39988d231cf70e52a5
Josfem-leeds <josp2013@gmail.com>
2022-07-07 18:17:12 -0700

    completely plaigerized

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op-add - add function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op-sub - sub function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns the subtraction
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op-mul - multiplication function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns the multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op-div - div function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op-mod - add function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: returns the modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
